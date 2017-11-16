4f39c67f652ddea8e70a9c401e34987ae4461b19
labuser <labuser@WPPB1546.ad.nottingham.ac.uk>
2017-11-16 12:51:16 +0000

    better training

--- net_setup.m
+++ net_setup.m
@@ -1,6 +1,6 @@
 imgSiz = 96;
 
-trainImgCnt = 100;
+trainImgCnt = 700;
 trainImg = zeros(imgSiz, imgSiz, 1, trainImgCnt);
 trainVal = zeros(1,1,1,trainImgCnt);
 
@@ -11,39 +11,47 @@ for i = 1:trainImgCnt
     trainVal(:,:,:,i) = Re;
 end
 
+figure
+idx = randperm(trainImgCnt,20);
+for i = 1:numel(idx)
+    subplot(4,5,i)
+
+    imshow(mat2gray(trainImg(:,:,:,i)))
+    title(sprintf('R_E = %f', trainVal(:,:,:,i)))
+    drawnow
+end
+
 disp('Preparing network structure...')
 layers = [ ...
     imageInputLayer([imgSiz imgSiz 1])
-    convolution2dLayer(12,96, 'Stride', 4)
+    convolution2dLayer(12,96, 'Stride', 8)
     reluLayer
     fullyConnectedLayer(1)
     regressionLayer];
 
-options = trainingOptions('sgdm','InitialLearnRate',0.001, ...
-    'MaxEpochs',20, 'Plots','training-progress');
+options = trainingOptions('sgdm','InitialLearnRate',0.005, 'MaxEpochs',30, 'Verbose', true);
 
 disp('Beginning Training...')
 net = trainNetwork(trainImg,trainVal,layers,options);
 
 disp('Testing...')
-testImgCnt = 200;
+testImgCnt = 1000;
 testImg = zeros(imgSiz, imgSiz, 1, testImgCnt);
-testVal = zeros(testImgCnt,1,1,1);
+testVal = zeros(1,1,1,trainImgCnt);
 
 disp('Generating test images...')
 for i = 1:testImgCnt
     Re = rand() * imgSiz * 0.3 + 6;
     testImg(:,:,1,i) = img_gen(imgSiz, Re);
-    testVal(i,:,:,:) = Re;
+    testVal(:,:,:,i) = Re;
 end
 
 
 predictedVal = predict(net, testImg);
 
-predictionError = testVal - predictedVal;
+predictionError = reshape(testVal, 1, []) - reshape(predictedVal, 1, []);
 
-thr = 10;
-numCorrect = sum(abs(predictionError) < thr);
-numTestImages = size(testImg,4);
+numCorrect = sum(abs(predictionError) < 2);
+accuracy = numCorrect / size(testImg,4)
 
-accuracy = numCorrect / testImgCnt
+
