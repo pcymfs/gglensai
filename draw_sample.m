a1e446f3e387b80331e3ccf926276c3f4847350d
Cubitect <cubitect@gmail.com>
2017-11-29 12:06:43 +0000

    added titles with network in/out values

--- draw_sample.m
+++ draw_sample.m
@@ -1,18 +1,27 @@
-function draw_sample(imgs,vals)
+function draw_sample(imgs,vals,net)
 
-disp('Drawing sample of training images...')
+disp('Drawing sample of images...')
 
-trainImgCnt = size(imgs,4);
-
-figure
-idx = randperm(trainImgCnt,20);
-for i = 1:numel(idx)
-    subplot(4,5,i)
-
-    imshow(mat2gray(imgs(:,:,:,i)))
-    title(sprintf('R_E = %f', vals(i)))
-    drawnow
+if exist('net', 'var')
+    predicted = predict(net, imgs);
 end
 
+figure();
+sx = 8;
+sy = 3;
+
+for y = 1:(sy)
+    for x = 0:(sx-1)
+        i = x * sy + y;
+        subplot('Position',[x/sx (sy-y-0.1)/sy 1/sx 1/sy])
+        imshow(mat2gray(imgs(:,:,:,i)));
+        if exist('predicted', 'var')
+            title(sprintf('R_E I:%.3f O:%.3f\ne  I:%.3f O:%.3f', ...
+                vals(i,1),predicted(i,1), vals(i,2), predicted(i,2)),'FontName', 'FixedWidth')
+        else
+            title(sprintf('R_E = %f\ne = %f', vals(i,1), vals(i,2)))
+        end
+    end
+    drawnow
 end
 
